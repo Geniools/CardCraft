79abb8ec807088d52c88838f8e92c56bc1f3763a
Geniools <30665942+Geniools@users.noreply.github.com>
2024-02-06 18:14:16 +0100

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CardCraft
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CardCraft
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
