dfc40145ca15669f22319f7783ae55248a637eaa
Geniools <30665942+Geniools@users.noreply.github.com>
2024-02-27 18:40:59 +0100

    Testing full screen

--- CardCraftClient/MauiProgram.cs
+++ CardCraftClient/MauiProgram.cs
@@ -2,6 +2,7 @@
 using CardCraftClient.ViewModel;
 using CommunityToolkit.Maui;
 using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
 
 namespace CardCraftClient;
 
@@ -22,8 +23,32 @@ public static class MauiProgram
             .RegisterViews()
             .RegisterViewModels();
 
+#if WINDOWS
+        builder.ConfigureLifecycleEvents(events =>
+        {
+            // Make sure to add "using Microsoft.Maui.LifecycleEvents;" in the top of the file 
+            events.AddWindows(windowsLifecycleBuilder =>
+            {
+                windowsLifecycleBuilder.OnWindowCreated(window =>
+                {
+                    window.ExtendsContentIntoTitleBar = false;
+                    var handle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                    var id = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(handle);
+                    var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(id);
+                    switch (appWindow.Presenter)
+                    {
+                        case Microsoft.UI.Windowing.OverlappedPresenter overlappedPresenter:
+                            overlappedPresenter.SetBorderAndTitleBar(false, false);
+                            overlappedPresenter.Maximize();
+                            break;
+                    }
+                });
+            });
+        });
+#endif
+
 #if DEBUG
-    	builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
         return builder.Build();
--- CardCraftClient/View/MainPage.xaml.cs
+++ CardCraftClient/View/MainPage.xaml.cs
@@ -1,25 +1,81 @@
-﻿namespace CardCraftClient
+﻿namespace CardCraftClient;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    public MainPage()
     {
-        int count = 0;
-
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+        InitializeComponent();
     }
 
+#if WINDOWS
+        private Microsoft.UI.Windowing.AppWindow GetAppWindow(MauiWinUIWindow window)
+        {
+            var handle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+            var id = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(handle);
+            var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(id);
+            return appWindow;
+        }
+#endif
+
+    private void OnToggleFullscreenClicked(object sender, EventArgs e)
+    {
+#if WINDOWS
+		var window = GetParentWindow().Handler.PlatformView as MauiWinUIWindow;
+        
+        var appWindow = GetAppWindow(window);
+
+        switch (appWindow.Presenter)
+        {
+            case Microsoft.UI.Windowing.OverlappedPresenter overlappedPresenter:
+                if (overlappedPresenter.State == Microsoft.UI.Windowing.OverlappedPresenterState.Maximized)
+                {
+                    overlappedPresenter.SetBorderAndTitleBar(true, true);
+                    overlappedPresenter.Restore();
+                }
+                else
+                {
+                    overlappedPresenter.SetBorderAndTitleBar(false, false);
+                    overlappedPresenter.Maximize();
+                }
+
+                break;
+        }
+#endif
+    }
+
+    // Still uncertain if we will need maximize and minimize functionality
+
+//     private void OnToggleMaximizeClicked(object sender, EventArgs e)
+//     {
+// #if WINDOWS
+//         var window = GetParentWindow().Handler.PlatformView as MauiWinUIWindow;
+//
+//         var appWindow = GetAppWindow(window);
+//
+//         switch (appWindow.Presenter)
+//         {
+//             case Microsoft.UI.Windowing.OverlappedPresenter overlappedPresenter:
+//                 overlappedPresenter.IsMaximizable = !overlappedPresenter.IsMaximizable;
+//                 break;
+//         }
+// #endif
+//     }
+//
+//     private void OnToggleMinimizeClicked(object sender, EventArgs e)
+//     {
+// #if WINDOWS
+//         var window = GetParentWindow().Handler.PlatformView as MauiWinUIWindow;
+//
+//         var appWindow = GetAppWindow(window);
+//
+//         switch (appWindow.Presenter)
+//         {
+//             case Microsoft.UI.Windowing.OverlappedPresenter overlappedPresenter:
+//                 overlappedPresenter.IsMinimizable = !overlappedPresenter.IsMinimizable;
+//                 break;
+//         }
+// #endif
+//     }
+
 }
+
