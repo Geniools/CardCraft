c2e93d8e963f62cf41c93f0b3704fc9a9b1b9774
KrystianWiazowskiV2 <89790188+bouncychair@users.noreply.github.com>
2024-03-08 17:42:07 +0100

    New StartPage View and ViewModel code

--- CardCraftClient/ViewModel/StartPageViewModel.cs
+++ CardCraftClient/ViewModel/StartPageViewModel.cs
@@ -1,12 +1,32 @@
 ﻿using CardCraftShared;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace CardCraftClient.ViewModel;
 
 public partial class StartPageViewModel : BaseViewModel
 {
+    [ObservableProperty]
+    private string _username;
+
+    [ObservableProperty]
+    private string _lobbyCode;
+
+    [ObservableProperty]
+    private string _gameName;
+
+    [ObservableProperty]
+    private string _title;
+
+    [ObservableProperty]
+    private bool _isEnabled;
+
     public StartPageViewModel(GameManager gm)
     {
-        //Title = "CardCraft"; 
+        this.GameName = "Card Craft";
+        this.Title = "Join or Create a Game:";
+        this.IsEnabled = true;
+        this.Username = "Chris";
+        this.LobbyCode = "773";
     }
 
 
