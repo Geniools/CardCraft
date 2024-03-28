ad343744ae30750bc17f3026e56a3683e622625b
KrystianWiazowskiV2 <89790188+bouncychair@users.noreply.github.com>
2024-03-28 11:21:08 +0100

    Added CardView Page and ViewModel

---
+++ CardCraftClient/ViewModel/CardViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using CardCraftShared.Cards.Minions;
+
+namespace CardCraftClient.ViewModel;
+
+public partial class CardViewModel : BaseViewModel
+{
+    private AlexCard _card;
+    public AlexCard Card
+    {
+        get => _card;
+        set
+        {
+            _card = value;
+            OnPropertyChanged(nameof(Card));
+        }
+    }
+
+    public CardViewModel()
+    {
+        Card = new AlexCard(); // Initialize your card with default values or load from somewhere
+    }
+
+    // Implement commands or methods here if needed
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
