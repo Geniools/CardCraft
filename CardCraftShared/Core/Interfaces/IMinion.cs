e05f411825dd683bd7fb3d8afc4ae0c4b30b94a9
Miroslav Penchev <miroslav.penchev@student.nhlstenden.com>
2024-04-07 20:28:29 +0200

    Update class diagram and remove unused code

--- CardCraftShared/Core/Interfaces/IMinion.cs
+++ CardCraftShared/Core/Interfaces/IMinion.cs
@@ -7,7 +7,6 @@ public interface IMinion : IBaseCard, IAttackable
     public int Health { get; set; }
     public int Attack { get; set; }
     public bool CanAttack { get; set; }
-    public event EventHandler OnDeath;
     public void TakeDamage(int damage);
     public void AttackMinion(IMinion minion);
     public void AttackHero(BaseHero hero);
